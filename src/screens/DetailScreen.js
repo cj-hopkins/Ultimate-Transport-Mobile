b2a541fa52ed614a6bb5739490b8bb684c6a0d8d
oleathlc <conor.lawlor@ucdconnect.ie>
2018-07-26 18:49:13 +0100

    Added button to navigate to map and to direction options

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -7,13 +7,20 @@ class DetailScreen extends Component {
     super(props);
     this.state = {
           routes: this.props.navigation.state.params.routes,
+          selectedRoute: null,
         }
   }
 
+  showOptions(value) {
+    this.setState({
+      selectedRoute: value
+    });
+  }
+
   render(){
-    const DEMO_OPTIONS_1 = [];
+    const busRoutes = [];
     for (var i =0; i < this.state.routes.length; i++){
-      DEMO_OPTIONS_1.push(this.state.routes[i].route)
+      busRoutes.push(this.state.routes[i].route)
     }
     console.log(this.state.routes);
 
@@ -23,9 +30,21 @@ class DetailScreen extends Component {
               onPress={() => this.props.navigation.navigate('Home')}
               title="Home"
               />*/}
-              <Text>Select Bus Route</Text>
-              <ModalDropdown options={DEMO_OPTIONS_1}
+              <ModalDropdown
+              defaultValue='Choose Route'
+              style={styles.dropdown_2}
+                           textStyle={styles.dropdown_2_text}
+                           dropdownStyle={styles.dropdown_2_dropdown}
+              onSelect={(value) => this.showOptions(value)}
+              options={busRoutes}
               />
+              <Button 
+              disabled ={this.state.selectedRoute===null}
+              title="Choose Direction"/>
+              <Button 
+              disabled ={this.state.selectedRoute===null}
+              onPress={() => this.props.navigation.navigate('Map')}
+              title="Show Route on Map"/>
         </View>
     );
   }
@@ -37,7 +56,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  }
+  },
+
+  dropdown_2: {
+    width: 150,
+    marginTop: 32,
+    right: 8,
+    borderWidth: 0,
+    borderRadius: 3,
+    backgroundColor: 'cornflowerblue',
+  },
+  dropdown_2_text: {
+    marginVertical: 10,
+    marginHorizontal: 6,
+    fontSize: 18,
+    color: 'white',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+  dropdown_2_dropdown: {
+    width: 150,
+    height: 300,
+    borderColor: 'cornflowerblue',
+    borderWidth: 2,
+    borderRadius: 3,
+  },
 })
 
 export default DetailScreen;

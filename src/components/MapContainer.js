3e12ec584f88cb1370cfd15c09620623dd2c370b
oleathlc <conor.lawlor@ucdconnect.ie>
2018-07-26 10:41:52 +0100

    Map asks user for location and sets at Dublin if access not granted

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,21 +1,90 @@
-import React from 'react';
-import { MapView } from 'expo';
-import { StyleSheet, Text, FlatList, ActivityIndicator, View, Image } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Constants, MapView, Location, Permissions } from 'expo';
 
+export default class App extends Component {
+  state = {
+    mapRegion: null,
+    hasLocationPermissions: false,
+    locationResult: null
+  };
 
-export default class MapContainer extends React.Component{
+  componentDidMount() {
+    this._getLocationAsync();
+  }
+
+  _handleMapRegionChange = mapRegion => {
+    console.log(mapRegion);
+    this.setState({ mapRegion });
+  };
+
+  _getLocationAsync = async () => {
+   let { status } = await Permissions.askAsync(Permissions.LOCATION);
+   if (status !== 'granted') {
+     this.setState({
+       hasLocationPermissions: true,
+       mapRegion: {
+          latitude: 53.3498,
+          longitude: -6.2603,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        },
+       locationResult: 'Dublin',
+     });
+   } else {
+     this.setState({ hasLocationPermissions: true });
+   }
+
+   let location = await Location.getCurrentPositionAsync({});
+   this.setState({ locationResult: JSON.stringify(location) });
+   
+   // Center the map on the location we just fetched.
+    this.setState({mapRegion: { latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }});
+  };
 
   render() {
-    return(
-      <MapView
-       style={{ flex: 1 }}
-       initialRegion={{
-         latitude: 37.78825,
-         longitude: -122.4324,
-         latitudeDelta: 0.0922,
-         longitudeDelta: 0.0421,
-       }}
-    />
-    )
+    return (
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>
+          Pan, zoom, and tap on the map!
+        </Text>
+        
+        {
+          this.state.locationResult === null ?
+          <Text>Finding your current location...</Text> :
+          this.state.hasLocationPermissions === false ?
+            <Text>Location permissions are not granted.</Text> :
+            this.state.mapRegion === null ?
+            <Text>Map region doesn't exist.</Text> :
+            <MapView
+              style={{ alignSelf: 'stretch', height: 400 }}
+              region={this.state.mapRegion}
+              onRegionChange={this._handleMapRegionChange}
+            />
+        }
+        
+        <Text>
+          Location: {this.state.locationResult}
+        </Text>
+      </View>
+        
+    );
+  }
 }
-}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
+  },
+});
